74504e6c52f38a0fedc73dc68ff9c253d02e5d77
Matthew Cornelisse <squarerootofnegativeone@gmail.com>
2019-02-12 08:54:02 -0600

    Made QR code clickable

--- js/digiid.js
+++ js/digiid.js
@@ -6,8 +6,13 @@ var DigiID=function(config,document,window,undefined) {
 	var digiQRmessage=config['digiid'];
 	var qrcode=config['qrcode'];
 	
-	if (digiQRmessage.legnth>127) console.error("Need to shorten uri must be less then 127 bytes currently: " +digiQRmessage.legnth)
-	document.getElementById(qrcode['id']).src=DigiQR.id(digiQRmessage,qrcode['size'],qrcode['logo'],1);
+	if (digiQRmessage.legnth>127) console.error("Need to shorten uri must be less then 127 bytes currently: " +digiQRmessage.legnth);
+	var domLoginQR=document.getElementById(qrcode['id']);
+	domLoginQR.src=DigiQR.id(digiQRmessage,qrcode['size'],qrcode['logo'],1);
+	domLoginQR.style.cursor="pointer";
+	domLoginQR.addEventListener('click',function() {
+		window.open(digiQRmessage, '_blank');
+	});
 	setInterval(function() {
 		xmr.getJSON('digiid/ajax.php?nonce='+config['nonce']).then(function(reqData){
             if(reqData!=false) {
